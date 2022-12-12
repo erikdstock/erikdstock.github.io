0646aebe8a39555d8b312c11e14e21020be6faf9
Erik Stockmeier <9088720+erikdstock@users.noreply.github.com>
2022-12-12 09:03:28 -0500

    Update layout with my own images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicons/apple-touch-icon.png"
+          />
           <link
             rel="icon"
             type="image/png"
@@ -25,7 +29,6 @@ class MyDocument extends Document {
             href="/static/favicons/favicon-16x16.png"
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
